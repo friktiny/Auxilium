15069c59e35e9e0883f022d5ca83b52db9d4b520
Nat76G33K <87605291+Nat76G33K@users.noreply.github.com>
2021-07-22 12:25:15 +0400

    Remove Package1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const configuration = require('./package1.json');
+const configuration = require('./package.json');
 var port = process.env.PORT || 80
 const client = new Discord.Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
